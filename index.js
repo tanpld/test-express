13a60fbe1c70bcca950eda90b469f05b54afc514
Tan Phan <tan.phan@alley51.com>
2019-06-19 22:17:24 +0700

    Styling

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
 app.use('/users', userRoute);
 
 app.set('view engine', 'pug');
