3fc03087f360bcb9c5ed46a6c1522f8c0aeaad92
Tan Phan <tan.phan@alley51.com>
2019-06-20 12:49:21 +0700

    Setup cookie

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const userRoute = require('./routes/user.route');
 
@@ -7,6 +8,8 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use(express.static('public'));
 
